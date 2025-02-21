a97828b8ba25c63c7ca184e34d93a44f3430a38c
Ramkumar <ramkumar.contactme@gmail.com>
2025-02-21 02:07:44 -0800

    multi threading- update

--- multi-threading/basic-threading2.py
+++ multi-threading/basic-threading2.py
@@ -13,15 +13,23 @@ def hi():
     time.sleep(2)
     print("woke up")
 
+def hi2():
+    print("Hi2")
+    print("Sleeping for 2 seconds")
+    time.sleep(2)
+    print("woke up")
+
 #create thread one
 t1 = Thread(target=hello)
 t2 = Thread(target=hi)
-
+t3 = Thread(target=hi2)
 start = time.time()
 t1.start()
 t2.start()
+t3.start()
 t1.join()
 t2.join()
+t3.join()
 end = time.time()
 
 print(end -start)
